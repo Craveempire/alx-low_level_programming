3fddd0bf2714e3467146b250ed51dab5d9638e57
Craveempire <olamilekanoluwafemi30@gmail.com>
2022-10-18 16:49:21 -0700

    Rufus

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -4,10 +4,10 @@
 /**
  * _putchar - writes the character c to stdout
  * @c: the character to print
-*
-* Return: on success 1
-*/
+ *
+ * Return: on success 1
+ */
 int _putchar(char c)
 {
-	return(writes(1,&c, 1));
+	return (write(1, &c, 1));
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-#ifndef MAiN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 void print_alphabet(void);
