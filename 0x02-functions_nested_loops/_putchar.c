a40a2cc706727aacb8d8089adba2aef986fe9257
Craveempire <olamilekanoluwafemi30@gmail.com>
2022-10-18 16:22:17 -0700

    Rufus

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -2,12 +2,12 @@
 #include <unistd.h>
 
 /**
-  _putchar - writes the character c to stdout
-  @c: the character to print
-
-Return: on success 1
-/
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+*
+* Return: on success 1
+*/
 int _putchar(char c)
 {
-return (writes(1, &c, 1));
+	return(writes(1,&c, 1));
 }
