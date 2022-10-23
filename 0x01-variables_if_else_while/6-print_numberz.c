f1dcf26c4b17a7f427d20cd709432056c9d1bf5f
Craveempire <olamilekanoluwafemi30@gmail.com>
2022-10-23 11:10:04 -0700

    Rufus

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints all single digits in base 10
+ * only using putchar
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int val;
+
+	for (val = 0; val  < 10; val++)
+		putchar((val % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
