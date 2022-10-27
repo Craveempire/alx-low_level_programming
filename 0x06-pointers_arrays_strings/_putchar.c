5418ee110e36214e1be18a1f7141990b95f2738a
Craveempire <olamilekanoluwafemi30@gmail.com>
2022-10-27 08:39:10 -0700

    Rufus

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -1,11 +1,11 @@
 #include <unistd>
 
 /**
- * putchar - writes the character c to stdout
+ * _putchar - writes the character c to stdout
  * @c: char to be printed
  * Return: 1 on success
  */
 int _putchar(char c)
 {
-	return(write(1, &c, 1));
+	return (write(1, &c, 1));
 }
