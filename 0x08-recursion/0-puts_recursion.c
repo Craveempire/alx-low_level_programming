80cb637269ccd4b48bcb5c47bbcf2db74861fe28
Craveempire <olamilekanoluwafemi30@gmail.com>
2022-11-02 12:32:09 -0700

    Rufus

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - A function that prints a string, followed by a new line.
+ * @s: A string to be printed
+ */
+void _puts_recursion(char *s)
+{
+	put(s);
+}
